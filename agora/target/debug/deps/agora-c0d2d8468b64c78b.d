ca70ad9c5b9478d3399d05d7ecd74bf8ac89064f
nlyu <lyuxingjian@gmail.com>
2025-09-13 17:44:14 -0400

    fix: Remove build artifacts and improve .gitignore
    - Add **/target/ to ignore all nested target directories
    - Remove accidentally committed agora/target/ build artifacts
    - Remove duplicate agora/agora/Cargo.lock (workspace manages locks)
    - Clean up repository from ~80 build artifact files

--- agora/target/debug/deps/agora-c0d2d8468b64c78b.d
+++
@@ -1,5 +0,0 @@
-/home/nlyu/Code/midas/agora/target/debug/deps/agora-c0d2d8468b64c78b.d: agora/src/main.rs
-
-/home/nlyu/Code/midas/agora/target/debug/deps/libagora-c0d2d8468b64c78b.rmeta: agora/src/main.rs
-
-agora/src/main.rs:
--- agora/target/debug/deps/process-e5e414bc42fa695b.d
+++
@@ -1,5 +0,0 @@
-/home/nlyu/Code/midas/agora/target/debug/deps/process-e5e414bc42fa695b.d: agora/tests/process.rs
-
-/home/nlyu/Code/midas/agora/target/debug/deps/libprocess-e5e414bc42fa695b.rmeta: agora/tests/process.rs
-
-agora/tests/process.rs:
